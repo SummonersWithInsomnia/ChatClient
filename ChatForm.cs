355851f6ae2b228d44b669de3ec43b316559a0be
Yu <yu@hiya.moe>
2024-09-30 14:14:08 -0600

    First commit

---
+++ ChatForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class ChatForm : Form
+    {
+        public ChatForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
